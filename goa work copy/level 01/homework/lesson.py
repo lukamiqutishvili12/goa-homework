8979cf441c61784b8fa3e245c745de32f1e23d38
lukamiqutishvili12 <miqutishvililuka27@gmail.com>
2024-11-21 20:34:49 +0400

    commit

---
+++ goa work copy/level 01/homework/lesson.py
@@ -0,0 +1,20 @@
+name = "luka"
+#name არის ცვლადი
+# = არის ცვლადისთვის მნიშვნელობის მიმნიჭებელი სიმბოლო 
+# "nika" არის ცვლადისთვის მნიშვნელობა 
+
+surname = "miqutishvili"
+
+print(name)
+#პრინტ ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+
+name = "luka"  #ეს არის str (string) ტიპის ცვლადი
+age = 17 # ეს არის int (integer) მთელი რიცხვი
+height = 1.75 #ეს არის float ტიპის ცვლადი (ათწილადი)
+#boolean (booL) ტიპის ცვლადი 
+
+knows__programming = True #True ან False
+print(name +  " " + str(age))
+
+
+
