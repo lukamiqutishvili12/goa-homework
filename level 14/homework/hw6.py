2bbc13e1d717fc9548436898a65f452a9d0f6d04
miqutishvililuka <luka.miqutishvili@icloud.com>
2025-02-19 23:55:08 +0400

    commit

---
+++ level 14/homework/hw6.py
@@ -0,0 +1,6 @@
+#შეამოწმეთ შეყვანილი სიტყვა hello-ა თუ hello world.
+name = input("enter a word")
+if name=="hello word":
+    print("hello word")
+    #სევამოწმეთ შეყვანილი სიტყვა hello-ა თუ hello world 
+    
